549626103f3441419bd8d9f3cbdd8846bc56573f
blakeypark <joshua.blake@sky.com>
2015-01-10 17:02:04 +0000

    Added message to hello2

--- src/hello2.java
+++ src/hello2.java
@@ -1,9 +1,9 @@
 
 public class hello2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println ("Hello World");
 	}
 
 }
