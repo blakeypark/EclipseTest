cf2315ba1e725ae7130d68e26b9e93c9c6077bd7
blakeypark <joshua.blake@sky.com>
2015-01-10 19:23:13 +0000

    I have added a for loop into the program to print all data in the array

--- src/sales/SalesMain.java
+++ src/sales/SalesMain.java
@@ -2,11 +2,15 @@ package sales;
 
 public class SalesMain {
 
-	int [] sales = {1, 10 , 20};
+	static int [] sales = {1, 10 , 20};
 	
 	public static void main(String[] args) 
 	{
 		System.out.println ("Sales program 2000");
+		for (int sale : sales)
+		{
+			System.out.println (sale);
+		}
 	}
 
 }
